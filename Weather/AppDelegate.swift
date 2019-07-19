5b3677c03251581539e8e1ab9fe05249501d9894
Sergei Prikhodko <dollar2048@gmail.com>
2019-07-19 09:46:31 +0200

    rearrange some files in groups

--- Weather/AppDelegate.swift
+++ Weather/AppDelegate.swift
@@ -15,15 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let weatherManager = WeatherManager()
     var startViewModel: CitiesViewModel!
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application (_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any] ?) -> Bool {
+
+
         Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/iOSInjection.bundle")!.load()
-        
+
         startViewModel = CitiesViewModel(weatherManager: weatherManager)
         if let navController = self.window?.rootViewController as? UINavigationController,
-            let startViewController = navController.topViewController  as? CitiesViewController {
+            let startViewController = navController.topViewController as? CitiesViewController {
             startViewController.viewModel = startViewModel
         }
-        
+
         return true
     }
 }
